33086e0bdf8a6dc360921f2148a98494c222d8f0
michielenzo <michielidema@outlook.com>
2019-02-16 02:57:29 +0100

    added a TODO for handling key events

--- src/main/kotlin/idema/michiel/game/GameView.java
+++ src/main/kotlin/idema/michiel/game/GameView.java
@@ -2,12 +2,15 @@ package idema.michiel.game;
 
 import idema.michiel.game.dto.PlayerDTO;
 import idema.michiel.game.dto.SendGameStateToClientsDTO;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
+import java.util.EventListener;
 import java.util.List;
 
 public class GameView extends Scene {
@@ -25,6 +28,11 @@ public class GameView extends Scene {
 
     private void gameCanvas(VBox root) {
         Canvas canvas = new Canvas(800, 500);
+        canvas.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent event) {
+                //TODO plan hier is om uiteindelijk een keyinput dto te maken en deze te verstgituren naar de server.
+            }
+        });
         ctx = canvas.getGraphicsContext2D();
         root.getChildren().add(canvas);
     }
